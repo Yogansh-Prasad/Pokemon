0767185685ff4230a2b8b5e5e3e7bfa5301f2ad0
Yogansh-Prasad <117001480+Yogansh-Prasad@users.noreply.github.com>
2024-10-26 13:50:10 +0530

    Setup

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string player_name;
+
+    cout << "Enter your name: ";
+    cin >> player_name;
+
+    cout << "Great Start " << player_name << ", Welcome to your journey ! " << endl;
+
+    return 0;
+}
